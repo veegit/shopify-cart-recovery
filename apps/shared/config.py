750b4f74cab173eab3b010fe42ad811be195b0bf
Vee <narayanan.vn@gmail.com>
2025-08-30 22:13:58 -0700

    second commit

--- apps/shared/config.py
+++ apps/shared/config.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional
-from pydantic import BaseSettings, validator
+from pydantic import validator
+from pydantic_settings import BaseSettings
 
 
 class RedisConfig(BaseSettings):
